0681b4726f9082dc98e44ed6f097d22516ba8ecb
TeChengHung <dan12411@gmail.com>
2016-11-04 19:51:09 +0800

    Initial Commit

---
+++ TeamUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeamUp
+//
+//  Created by 洪德晟 on 2016/11/4.
+//  Copyright © 2016年 洪德晟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
